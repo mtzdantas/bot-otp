8f6a76d1b0444da148042008ecd06bb3b84e80b2
Mateus Dantas <mtzdantas@gmail.com>
2025-04-01 15:13:44 -0300

    Refactor position capturing logic and enhance user interaction; implement keyboard controls for capturing and exiting

--- cavebot.py
+++ cavebot.py
@@ -1,15 +1,24 @@
 import pyautogui
+import keyboard
 
 try:
   posicoes = []
-  arq = input('Qual nome do arquivo?')
-  while True:
-    option = pyautogui.confirm("Selecione OK para começar e clique na posição desejada!")
-    if option == 'OK':
-      x, y = pyautogui.position()
-    posicoes.append((x, y))
-    with open(f'{arq}.txt', 'a') as f:
-      f.write(f"{x},{y}\n")
+  arq = input('Qual nome do arquivo? ')
+  option = pyautogui.confirm("Pressione OK para iniciar.\nPressione 'C' para capturar a posição do mouse ou 'ESC' para sair.")
+  if option == 'OK':
+    while True:
+      keyboard.read_event()
+      if keyboard.is_pressed('esc'):
+        break
+      if keyboard.is_pressed('c'):
+        x, y = pyautogui.position()
+        posicoes.append((x, y))
+        print(f"Posição capturada: ({x}, {y})")
+              
+    with open(f'{arq}.txt', 'w') as f:
+      f.write(str(posicoes))
+         
+  else:
+    print("Operação cancelada.")
 except KeyboardInterrupt:
   print("\nCaptura de posições interrompida.")
-  print(f"Total de {len(posicoes)} posições salvas em '{arq}.txt'.")
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from classes import setup
 from classes import troca
-import keyboard
+from keyboard import is_pressed
 import pyautogui
-import time
-
-def battle():
+from time import sleep
+from ast import literal_eval
+def check_state():
   try:
     check = pyautogui.locateOnScreen('battle.png', confidence=0.9)
     if check:
+      # cavebot()
       pass
 
   except pyautogui.ImageNotFoundException:
-    time.sleep(1)
-    pyautogui.press(['Q', 'E'])
-    troca.ida()
+    battle()
+  
+def battle():
+  pyautogui.press(['Q', 'E'])
+  sleep(0.5)
+  troca.ida()
+
+def cavebot():
+  global indice
+  if indice < len(posicoes):
+    x, y = posicoes[indice]
+    pyautogui.click(x, y)
+    sleep(1)
+    indice += 1
+
+  else:QE
+    indice = 0
+    print("Fim do percurso.")
 
 setup = setup()
 
@@ -21,9 +37,13 @@ battle_box = setup.set_battle()
 pyautogui.screenshot('battle.png', region=battle_box)
 pokes_pos = setup.set_pokesequence()
 troca = troca(pokes_pos=pokes_pos)
+# arquivo = input('Qual nome do arquivo? ')
+# with open(f'{arquivo}.txt', 'r') as f:
+  # conteudo = f.read().strip()
+  # posicoes = literal_eval(conteudo)
+# indice = 0
 
 while True:
-  if keyboard.is_pressed('P'):
+  if is_pressed('P'):
     break
-
-  battle()
+  check_state()
